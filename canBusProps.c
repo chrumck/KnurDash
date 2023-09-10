b1d0b7f9ce2206143aec5ecbba1d456ca08aa103
Knur In Charge <chrumck@gmail.com>
2023-09-10 20:16:41 +0200

    update coolant temp rawMin and rawMax

--- canBusProps.c
+++ canBusProps.c
@@ -79,7 +79,7 @@ static const CanSensor canSensors[CAN_SENSORS_COUNT] = {
             .labelId = "coolantTemp", .frameId = "coolantTempFrame", .labelMinId = "coolantTempMin", .labelMaxId = "coolantTempMax",
             .alertLow = -30, .warningLow = -30, .notifyLow = 80,
             .notifyHigh = 110, .warningHigh = 110, .alertHigh = 120,
-            .rawMin = -30, .rawMax = 224,
+            .rawMin = -40, .rawMax = 215,
             .format = "%.0f" , .precision = 0.3,
         },
             .getValue = getCoolantTemp,
