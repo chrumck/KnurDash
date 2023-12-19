935c0a3ac33c398dabc8542661183eb7ed1be635
Knur In Charge <chrumck@gmail.com>
2023-12-19 15:43:07 +0100

    Add volatile keyword to AppData

--- appData.c
+++ appData.c
@@ -5,7 +5,7 @@
 
 #include "dataContracts.h"
 
-static AppData appData = {
+static volatile AppData appData = {
     .canBus = {.i2cPiHandle = -1, .i2cCanHandle = -1},
     .sensors = {.i2cPiHandle = -1, .i2cAdcHandles = {-1, -1}}
 };
