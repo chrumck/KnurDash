61d78c378d89f139ddb39cf239effae0af7c01c3
Knur In Charge <chrumck@gmail.com>
2024-05-02 20:43:24 +0200

    Add trans pump switching logic

--- systemWorker.c
+++ systemWorker.c
@@ -7,11 +7,65 @@
 #define SYSTEM_WORKER_LOOP_INTERVAL_US 25000
 #define SHUTDOWN_DELAY 600
 #define SHUTDOWN_DELAY_AFTER_ENGINE_STARTED 30
-#define OIL_PRESSURE_ALERT_MIN_RPM 1300
 
 #define IGN_GPIO_PIN 22
 #define TRANS_PUMP_GPIO_PIN 23
 #define BUZZER_GPIO_PIN 27
+
+#define OIL_PRESSURE_MIN 0.5
+#define OIL_PRESSURE_ALERT_MIN_RPM 1300
+#define ENGINE_RPM_MIN 100
+
+#define TRANS_PUMP_ON_TEMP_C 100.0
+#define TRANS_PUMP_ON_MAX_TIME_US 20000000
+#define TRANS_PUMP_ON_MAX_CYCLES (TRANS_PUMP_ON_MAX_TIME_US / SYSTEM_WORKER_LOOP_INTERVAL_US)
+#define TRANS_PUMP_OFF_TEMP_C 70.0
+#define TRANS_PUMP_OFF_MAX_TIME_US 300000000
+#define TRANS_PUMP_OFF_MAX_CYCLES (TRANS_PUMP_OFF_MAX_TIME_US / SYSTEM_WORKER_LOOP_INTERVAL_US)
+
+gdouble getAdcSensorValue(gint adc, gint channel) {
+    SensorReading* reading = &(appData.sensors.adcReadings)[adc][channel];
+    const AdcSensor* sensor = &adcSensors[adc][channel];
+
+    return reading->errorCount < SENSOR_WARNING_ERROR_COUNT ?
+        reading->value : sensor->base.defaultValue;
+}
+
+void switchTransPumpOnOff(gint i2cPiHandle, guint* transPumpCounter) {
+    (*transPumpCounter)++;
+
+    gboolean isTransPumpOn = gpio_read(i2cPiHandle, TRANS_PUMP_GPIO_PIN) == FALSE;
+    gdouble transTempValue = getAdcSensorValue(TRANS_TEMP_ADC, TRANS_TEMP_CHANNEL);
+
+    if (isTransPumpOn && !appData.system.isEngineRunning) {
+        gpio_write(i2cPiHandle, TRANS_PUMP_GPIO_PIN, TRUE);
+        *transPumpCounter = 0;
+        return;
+    }
+
+    if (!appData.system.isEngineRunning) return;
+
+    if (!isTransPumpOn && transTempValue >= TRANS_PUMP_ON_TEMP_C) {
+        gpio_write(i2cPiHandle, TRANS_PUMP_GPIO_PIN, FALSE);
+        *transPumpCounter = 0;
+        return;
+    }
+
+    if (isTransPumpOn && transTempValue >= TRANS_PUMP_ON_TEMP_C) {
+        return;
+    }
+
+    if (!isTransPumpOn && transTempValue >= TRANS_PUMP_OFF_TEMP_C && *transPumpCounter > TRANS_PUMP_OFF_MAX_CYCLES) {
+        gpio_write(i2cPiHandle, TRANS_PUMP_GPIO_PIN, FALSE);
+        *transPumpCounter = 0;
+        return;
+    }
+
+    if (isTransPumpOn && *transPumpCounter > TRANS_PUMP_ON_MAX_CYCLES) {
+        gpio_write(i2cPiHandle, TRANS_PUMP_GPIO_PIN, TRUE);
+        *transPumpCounter = 0;
+    }
+}
 
 gpointer systemWorkerLoop() {
     while (!appData.shutdownRequested && !appData.isSensorWorkerRunning) { g_usleep(SYSTEM_WORKER_LOOP_INTERVAL_US); }
@@ -46,8 +100,7 @@ gpointer systemWorkerLoop() {
     if (gpioResult != 0) g_error("Could not set GPIO pin state for trans pump: %d", gpioResult);
 
     guint shutDownCounter = 0;
-    gboolean buzzerOn = FALSE;
-    gint32 engineRpm = 0;
+    guint transPumpCounter = 0;
 
     while (!appData.shutdownRequested && appData.isSensorWorkerRunning) {
         float errorRate = (float)appData.sensors.errorCount / appData.sensors.requestCount;
@@ -60,12 +113,12 @@ gpointer systemWorkerLoop() {
 
         appData.system.isIgnOn = gpio_read(i2cPiHandle, IGN_GPIO_PIN);
 
-        SensorReading* pressureReading = &(appData.sensors.adcReadings)[OIL_PRESS_ADC][OIL_PRESS_CHANNEL];
-        const AdcSensor* pressureSensor = &adcSensors[OIL_PRESS_ADC][OIL_PRESS_CHANNEL];
-        gdouble pressureValue = pressureReading->errorCount < SENSOR_WARNING_ERROR_COUNT ?
-            pressureReading->value : pressureSensor->base.defaultValue;
+        gdouble pressureValue = getAdcSensorValue(OIL_PRESS_ADC, OIL_PRESS_CHANNEL);
+        gdouble engineRpm = getEngineRpm();
 
-        appData.system.isEngineRunning = appData.system.isIgnOn && pressureValue > pressureSensor->base.alertLow;
+        appData.system.isEngineRunning = appData.system.isIgnOn &&
+            (pressureValue > OIL_PRESSURE_MIN || engineRpm > ENGINE_RPM_MIN);
+
         if (!appData.system.wasEngineStarted && appData.system.isEngineRunning) { appData.system.wasEngineStarted = TRUE; }
 
         guint32* coolantTempReadingErrorCount = &(appData.sensors.canReadings[COOLANT_TEMP_CAN_SENSOR_INDEX].errorCount);
@@ -79,18 +132,20 @@ gpointer systemWorkerLoop() {
             appData.canBusRestartRequested = TRUE;
         }
 
-        buzzerOn = gpio_read(i2cPiHandle, BUZZER_GPIO_PIN) == TRUE;
-        engineRpm = getEngineRpm();
+        gboolean isBuzzerOn = gpio_read(i2cPiHandle, BUZZER_GPIO_PIN) == TRUE;
+        const AdcSensor* pressureSensor = &adcSensors[OIL_PRESS_ADC][OIL_PRESS_CHANNEL];
 
-        if (buzzerOn && (pressureValue >= pressureSensor->base.alertLow || engineRpm < OIL_PRESSURE_ALERT_MIN_RPM)) {
+        if (isBuzzerOn && (pressureValue >= pressureSensor->base.alertLow || engineRpm < OIL_PRESSURE_ALERT_MIN_RPM)) {
             gpio_write(i2cPiHandle, BUZZER_GPIO_PIN, FALSE);
         }
 
-        if (!buzzerOn &&
+        if (!isBuzzerOn &&
             pressureValue < pressureSensor->base.alertLow &&
             engineRpm > OIL_PRESSURE_ALERT_MIN_RPM) {
             gpio_write(i2cPiHandle, BUZZER_GPIO_PIN, TRUE);
         }
+
+        switchTransPumpOnOff(i2cPiHandle, &transPumpCounter);
 
 #ifndef IS_DEBUG
         shutDownCounter = appData.system.isIgnOn ? 0 : shutDownCounter + 1;
